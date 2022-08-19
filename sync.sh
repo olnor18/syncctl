olnor18/syncctl

commit 96a28d16dd08df8296dec3c158c870a53503108d

Support Flux instead of Yggdrasil[1]/Argo CD
We are moving away from Yggdrasil/Argo CD to Flux as it better meets our
needs. One of the main reasons is better support for multi tenancy[2].

Git repositories (GitRepository[3]) specified in Flux aren't packaged,
as you are expected to run syncctl on every individuel repository (ex:
to support different release cadence in a multi tenancy[2] setup).

[1] https://github.com/distributed-technologies/yggdrasil
[2] https://github.com/fluxcd/flux2-multi-tenancy
[3] https://fluxcd.io/docs/components/source/gitrepositories/

sync.sh
@@ -50,7 +50,8 @@ sync_git() {
   kubectl -n support port-forward service/git-server-service 1234:80 >/dev/null &
   sleep 1
 
-  git -C yggdrasil push --force http://localhost:1234/git-test-project.git
+  repo="$(jq -r '.flux_repository.repository' -r manifest.json | cut -f 3- -d /)"
+  git -C yggdrasil push --force "http://localhost:1234/${repo}"
 
   jobs -p | xargs --no-run-if-empty kill
   trap -- EXIT

manifest.json.sample
@@ -1,60 +1,103 @@
 {
     "charts": [
         {
-            "chart": "nidhogg",
-            "digest": "af4901f2568b7ab123fafcd76ac4d6a99337e8f1be5dca24bf4fcd1280a054a4",
-            "version": "1.0.16"
+            "chart": "podinfo",
+            "digest": "4578752316dc3cb0aacc073171bc97f05465e650c5849b7faa2d530bb74cc6cc",
+            "version": "6.1.6"
         },
         {
-            "chart": "lightvessel",
-            "digest": "cbd24f12e42743ea0dee82c546e570b29d3fcccc2ee14709cf05730afcec25fc",
-            "version": "2.0.3"
-        },
-        {
-            "chart": "rook",
-            "digest": "0d9c16a988d6e0dfd6d4b02173d6334e61c122d1825c4f1467253b418a93f1c9",
-            "version": "0.1.3"
+            "chart": "rook-ceph",
+            "digest": "2e886e71f82bc997c2294e972cbce13df3d3e4ac0c17bcc3b27ff66bdfae1070",
+            "version": "v1.9.7"
         },
         {
             "chart": "rook-ceph-cluster",
-            "digest": "3f79af13669560572aeec4ad647440e70aab817a598753e47365fd0e7cb77ec2",
-            "version": "0.1.6"
+            "digest": "dfad0574738446bec3826e0aeecd171677b4f1df97e81b310c8ed42e06c13ed5",
+            "version": "v1.9.7"
+        },
+        {
+            "chart": "kube-prometheus-stack",
+            "digest": "d6eb7dde2624a174329e4bafc8b27d77279795ef1872533ac15804b7a7561ca0",
+            "version": "37.2.0"
         }
     ],
+    "flux_repository": {
+        "commit": "c41e80920182f4dc44f36743c57f630ce0eabc57",
+        "entrypoint": "clusters/dev/flux-system",
+        "repository": "https://github.com/distributed-technologies/flux-test.git"
+    },
     "images": [
         {
-            "digest": "sha256:0f4f3e1068268f0f5192797b003add304d9af5030b8ff341a7a716759d6102c7",
-            "image": "library/haproxy",
-            "registry": "docker.io",
-            "tag": "2.0.4"
-        },
-        {
-            "digest": "sha256:2e0e725c9282bfb7b08bea7dbbfd8dbce6410d670e3f8addd9b6540d818ad520",
-            "image": "argoproj/argocd",
-            "registry": "quay.io",
-            "tag": "v2.1.2"
-        },
-        {
-            "digest": "sha256:6b3cc1c385fbc7542244614e4432f2546c619b7850d44d2379c598309a06bed8",
-            "image": "dexidp/dex",
+            "digest": "sha256:3614ec75eb7e90356fdfcea00aaa6bf97a1b5b305a0ed9558f7dfab9ed43010c",
+            "image": "fluxcd/helm-controller",
             "registry": "ghcr.io",
-            "tag": "v2.30.0"
+            "tag": "v0.21.0"
         },
         {
-            "digest": "sha256:8061ca607db2a0c80010aeb5fc9bed0253448bc68711eaa14253a392f6c48280",
-            "image": "library/redis",
-            "registry": "docker.io",
-            "tag": "6.2.4-alpine"
+            "digest": "sha256:c9e8264460b79ef9aabfc67b5eca6994c01cd4bac684180902bab4332d85c651",
+            "image": "fluxcd/kustomize-controller",
+            "registry": "ghcr.io",
+            "tag": "v0.25.0"
         },
         {
-            "digest": "sha256:f3f51453e4261734f08579fe9c812c66ee443626690091401674be4fb724da70",
-            "image": "oliver006/redis_exporter",
+            "digest": "sha256:0aeb57c1d328114effee005de25d1871e834c09fde12441f6f2dc2bae1bdfbc8",
+            "image": "fluxcd/notification-controller",
+            "registry": "ghcr.io",
+            "tag": "v0.23.5"
+        },
+        {
+            "digest": "sha256:2b89709e7ff8cdda056d0c2e2a06e73a207a2e37ce916041de158d13a348d822",
+            "image": "fluxcd/source-controller",
+            "registry": "ghcr.io",
+            "tag": "v0.24.4"
+        },
+        {
+            "digest": "sha256:45dde4968305074d15a975f4897cd696d254a952e3bba0d5db91059641a162ff",
+            "image": "stefanprodan/podinfo",
+            "registry": "ghcr.io",
+            "tag": "6.1.6"
+        },
+        {
+            "digest": "sha256:c00ba61e2d7940413feb9d060a3df64d3b5010e01bdf563752cd8c4ba5ebde13",
+            "image": "rook/ceph",
             "registry": "docker.io",
-            "tag": "v1.15.1"
+            "tag": "v1.9.7"
+        },
+        {
+            "digest": "sha256:d5b2a2e2bb07a4a5a7c4bd9e54641cab63e1d2627622dbde17efc04849d3d30d",
+            "image": "prometheus/node-exporter",
+            "registry": "quay.io",
+            "tag": "v1.3.1"
+        },
+        {
+            "digest": "sha256:ef2a72bc0bc150ffb24cc8ea65ed4e6900c9a88753d9b18d1e1e89c77cc66efc",
+            "image": "kiwigrid/k8s-sidecar",
+            "registry": "quay.io",
+            "tag": "1.19.2"
+        },
+        {
+            "digest": "sha256:1e180fcaede581b8cc8e2ce8bc010daff9c64972023a0faaf226d92bd9840113",
+            "image": "grafana/grafana",
+            "registry": "docker.io",
+            "tag": "9.0.2"
+        },
+        {
+            "digest": "sha256:8f5d17635bcfcf49186154b9745e08015879ef1c01853f1ff74366db5da4137b",
+            "image": "kube-state-metrics/kube-state-metrics",
+            "registry": "registry.k8s.io",
+            "tag": "v2.5.0"
+        },
+        {
+            "digest": "sha256:1c190491ddbdc16e4a868dc8f575e34f48a1739656f53ef5179cac4a7252feff",
+            "image": "prometheus-operator/prometheus-operator",
+            "registry": "quay.io",
+            "tag": "v0.57.0"
+        },
+        {
+            "digest": "sha256:23a03c9c381fba54043d0f6148efeaf4c1ca2ed176e43455178b5c5ebf15ad70",
+            "image": "ingress-nginx/kube-webhook-certgen",
+            "registry": "k8s.gcr.io",
+            "tag": "v1.1.1"
         }
-    ],
-    "yggdrasil_repository": {
-        "commit": "00d6a62598bc7833bde3e067043fe842bbbccbbf",
-        "repository": "https://github.com/distributed-technologies/yggdrasil.git"
-    }
+    ]
 }

syncctl.py
@@ -10,6 +10,9 @@ import shutil
 import tarfile
 import datetime
 import tempfile
+import collections
+import re
+import itertools
 from pathlib import Path
 from argparse import ArgumentParser
 from logging import basicConfig
@@ -26,8 +29,8 @@ parser.add_argument(
 )
 subcommands = parser.add_subparsers(dest="subcommand")
 ci_parser = subcommands.add_parser(
-    "mirror-yggdrasil",
-    help="mirror the yggdrasil git repository to the local fs",
+    "mirror-flux",
+    help="mirror the flux git repository to the local fs",
 )
 ci_parser = subcommands.add_parser(
     "mirror-charts",
@@ -48,14 +51,14 @@ ci_parser = subcommands.add_parser(
     help="create a tarball",
 )
 
-def mirror_yggdrasil(yggdrasil_config: dict) -> None:
-    if not Path("work/yggdrasil").is_dir():
-        repo = git.Repo.clone_from(yggdrasil_config["repository"], "work/yggdrasil")
+def mirror_flux(flux_config: dict) -> None:
+    if not Path("work/flux").is_dir():
+        repo = git.Repo.clone_from(flux_config["repository"], "work/flux")
     else:
-        repo = git.Repo('work/yggdrasil')
-    if repo.head.object.hexsha != yggdrasil_config["commit"]:
+        repo = git.Repo('work/flux')
+    if repo.head.object.hexsha != flux_config["commit"]:
         repo.git.fetch()
-        repo.git.reset('--hard', yggdrasil_config["commit"])
+        repo.git.reset('--hard', flux_config["commit"])
 
 def download_file(url: str, dest: str, hash: str = None) -> None:
     with requests.get(url) as r:
@@ -101,37 +104,89 @@ def download_chart(name: str, version: str, repository: str) -> dict:
 
     raise Exception(f'Chart: {name}:{version} not found in {repository}')
 
-def download_dependencies(chart: str) -> list[str]:
-    dependencies = []
-    f = open(f"work/yggdrasil/{chart}/Chart.yaml")
-    index = yaml.load(f.read(), Loader=yaml.SafeLoader)
-    for chart in index["dependencies"]:
-        dependencies.append(download_chart(chart["name"], chart["version"], chart["repository"]))
-    return dependencies
+def template_flux(root_dir: str, git_repo: str, dir: str, git_repos: dict = collections.defaultdict(dict)) -> str:
+    p = subprocess.run(["kubectl", "kustomize", dir], capture_output=True, text=True)
+    if p.returncode != 0:
+        raise Exception(f'Error templating flux, dir: {dir}, error: {p.stderr}')
+
+    # Return early if no manifest was outputted
+    if p.stdout == '':
+        return p.stdout
+
+    kustomizations = []
+
+    # https://regex101.com/r/AHQNR4/1
+    pattern = re.compile('^(https?|ssh)://(.*@)?([^/:]*)[^/]*(.*)$')
+    git_repo = pattern.sub(r'\3\4', git_repo)
+
+    documents = yaml.load_all(p.stdout, Loader=yaml.SafeLoader)
+    for document in documents:
+        if document["kind"] == "GitRepository":
+            repo = document.get('spec').get('url')
+            repo = pattern.sub(r'\3\4', repo)
+            if repo == git_repo:
+                metadata = document.get('metadata')
+                git_repos[metadata.get('namespace')][metadata.get('name')] = repo
+        elif document["kind"] == "Kustomization":
+            kustomizations.append(document)
+
+    manifests = p.stdout
+    for kustomization in kustomizations:
+        spec = kustomization.get('spec')
+        source_ref = spec.get('sourceRef')
+        namespace = kustomization.get('metadata').get('namespace')
+        git_name = source_ref.get('name')
+        git_namespace = source_ref.get('namespace', namespace)
+        if git_namespace in git_repos and git_name in git_repos.get(git_namespace):
+            new_dir = os.path.normpath(f"{root_dir}/{spec.get('path')}")
+            # Prevents looping
+            if new_dir == dir:
+                continue
+            new_manifests = template_flux(root_dir, git_repo, new_dir, git_repos)
+            if new_manifests != '':
+                manifests += '---\n'
+                manifests += new_manifests
+
+    return manifests
 
 def mirror_charts(manifest: dict, manifest_file: str) -> None:
-    if not Path("work/yggdrasil").is_dir():
-        raise Exception('Please run mirror-yggdrasil before mirror-charts')
-    for dir in ["work/helm-chart-repo.tmp", "work/yggdrasil/yggdrasil/charts"]:
+    if not Path("work/flux").is_dir():
+        raise Exception('Please run mirror-flux before mirror-charts')
+    for dir in ["work/helm-chart-repo.tmp", "work/flux/flux/charts"]:
         if Path(dir).is_dir():
             shutil.rmtree(dir)
         os.makedirs(dir)
 
-    charts = []
-    charts.extend(download_dependencies("nidhogg"))
-    for dependency in download_dependencies("yggdrasil"):
-        charts.append(dependency)
-        shutil.copyfile(f"work/helm-chart-repo.tmp/{dependency['chart']}-{dependency['version']}.tgz", f"work/yggdrasil/yggdrasil/charts/{dependency['chart']}-{dependency['version']}.tgz")
+    manifests = template_flux("work/flux", manifest["flux_repository"]["repository"], "work/flux/" + manifest["flux_repository"]["entrypoint"])
 
-    p = subprocess.run(["helm", "template", "--set=loadbalancer.ipRangeStart=127.0.0.1", "work/yggdrasil/yggdrasil"], capture_output=True)
-    if p.returncode != 0:
-        raise Exception(f'Error templating yggdrasil, error: {p.stderr}')
-    documents = yaml.load_all(p.stdout, Loader=yaml.SafeLoader)
+    helm_repos = collections.defaultdict(dict)
+    helm_charts = []
+
+    documents = yaml.load_all(manifests, Loader=yaml.SafeLoader)
     for document in documents:
-        if document["apiVersion"] == "argoproj.io/v1alpha1" and document["kind"] == "Application" and 'chart' in document["spec"]["source"]:
-            source = document["spec"]["source"]
-            chart = download_chart(source["chart"], source["targetRevision"], source["repoURL"])
-            charts.append(chart)
+        if document["kind"] == "HelmRepository":
+            metadata = document.get('metadata')
+            helm_repos[metadata.get('namespace')][metadata.get('name')] = document.get('spec').get('url')
+        elif document["kind"] == "HelmRelease":
+            chart_spec = document.get('spec').get('chart').get('spec')
+            values = document.get('spec').get('values')
+            helm_charts.append({
+                "chart": chart_spec.get('chart'),
+                "version": chart_spec.get('version'),
+                "helm_repo": {
+                    "namespace": chart_spec.get('sourceRef').get('namespace'),
+                    "name": chart_spec.get('sourceRef').get('name'),
+                },
+                "values": values
+            })
+
+    charts = []
+    for chart in helm_charts:
+        helm_repo_namespace = chart.get('helm_repo').get('namespace')
+        helm_repo_name = chart.get('helm_repo').get('name')
+        helm_repo = helm_repos.get(helm_repo_namespace).get(helm_repo_name)
+        chart = download_chart(chart.get('chart'), str(chart.get('version') or ''), helm_repo)
+        charts.append(chart)
 
     p = subprocess.run(["helm", "repo", "index", "work/helm-chart-repo.tmp"], capture_output=True)
     if p.returncode != 0:
@@ -285,10 +340,13 @@ def resolve_images(manifest: dict, manifest_file: str) -> None:
         for image in helm_config["extra_images"]:
             if image not in images:
                 images[image] = process_image(image)
-    for m in template_charts(helm_config.get("api_versions", []), helm_config.get("values", {})):
+
+    manifests = template_flux("work/flux", manifest["flux_repository"]["repository"], "work/flux/" + manifest["flux_repository"]["entrypoint"])
+    for m in itertools.chain(iter([manifests]), template_charts(helm_config.get("api_versions", []), helm_config.get("values", {}))):
         for image in extract_images(m):
             if image not in images:
                 images[image] = process_image(image)
+
     images = list(images.values())
     for image in manifest.get("images", []):
         # FIXME: This isn't scalable
@@ -301,9 +359,10 @@ def resolve_images(manifest: dict, manifest_file: str) -> None:
     manifest["images"] = images
     save_manifest(manifest, manifest_file)
 
-def tar() -> None:
+def tar(manifest_file: str) -> None:
     name = datetime.datetime.now(datetime.timezone.utc).isoformat(timespec='seconds')
     with tarfile.open(f"{name}.tar", "w") as tar:
+        tar.add(manifest_file, arcname=f"{name}/manifest.json")
         tar.add("sync.sh", arcname=f"{name}/sync.sh")
         tar.add("work", arcname=name)
     print(f"Created {name}.tar")
@@ -328,8 +387,8 @@ def main() -> None:
     if args.verbose:
         basicConfig(level=DEBUG)
 
-    if "mirror-yggdrasil" == args.subcommand:
-        mirror_yggdrasil(manifest["yggdrasil_repository"])
+    if "mirror-flux" == args.subcommand:
+        mirror_flux(manifest["flux_repository"])
     elif "mirror-charts" == args.subcommand:
         mirror_charts(manifest, manifest_file)
     elif "mirror-images" == args.subcommand:
@@ -337,7 +396,7 @@ def main() -> None:
     elif "resolve-images" == args.subcommand:
         resolve_images(manifest, manifest_file)
     elif "tar" == args.subcommand:
-        tar()
+        tar(manifest_file)
     else:
         parser.print_help()
 

commit 22d3aa18bed51d473f251169c71408dea198b787

Avoid hard coding the git repos by creating them on-demand
In [1] the git repositories was changed to be namespaced, but the git
server doesn't support creating a new git repository on push, and we
don't want to hardcode the list of git repositories. So tweak the
sync.sh script to create the git repository on disk, if it doesn't
exist. Also fix wrong "git path" which should have been adjusted in[1].

[1] 96a28d1 ("Support Flux instead of Yggdrasil[1]/Argo CD")

Fixes: 96a28d1 ("Support Flux instead of Yggdrasil[1]/Argo CD")

sync.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -o nounset -o errexit
+namespace="support"
 
 sync_image() {
   skopeo sync --all --dest-tls-verify=false --src dir --dest docker "images/${1}/${2}" "localhost:5000/${1}/${3}"
@@ -35,7 +36,6 @@ sync_images() {
 }
 
 sync_helm() {
-  namespace="support"
   pvc="helm-registry-data-pvc"
   vol_name="$(kubectl -n ${namespace} get pvc ${pvc} -o jsonpath="{.spec.volumeName}")"
 
@@ -45,18 +45,34 @@ sync_helm() {
   rsync -v -aP --delete helm-chart-repo/ "$helm_repo_data_dir"
 }
 
+# Credit: https://stackoverflow.com/a/37840948
+urldecode() {
+  "${*//+/ }"
+  echo -e "${_//%/\\x}"
+}
+
 sync_git() {
+  pvc="git-server-data-pvc"
+  vol_name="$(kubectl -n ${namespace} get pvc ${pvc} -o jsonpath="{.spec.volumeName}")"
+
+  git_repo_data_dir="$(compgen -G "/var/lib/docker/volumes/*/_data/local-path-provisioner/${vol_name}_${namespace}_${pvc}")"
+  repo="$(jq -r '.flux_repository.flux_repository' -r manifest.json | cut -f 3- -d / | cut -f 2 -d @)"
+  git_repo_dir="${git_repo_data_dir}/$(urldecode "${repo}")"
+  if [ ! -d "${git_repo_dir}" ]; then
+    mkdir -p "${git_repo_dir}"
+    git -C "${git_repo_dir}" init --bare
+    chown -R 1000:1000 "${git_repo_data_dir}/$(cut -f1 -d / <<< "${repo}")"
+  fi
+
   trap "jobs -p | xargs --no-run-if-empty kill" EXIT
   kubectl -n support port-forward service/git-server-service 1234:80 >/dev/null &
   sleep 1
 
-  repo="$(jq -r '.flux_repository.repository' -r manifest.json | cut -f 3- -d /)"
-  git -C yggdrasil push --force "http://localhost:1234/${repo}"
+  git -C flux push --force "http://localhost:1234/${repo}"
 
   jobs -p | xargs --no-run-if-empty kill
   trap -- EXIT
 }
-
 
 main() {
   sync_images